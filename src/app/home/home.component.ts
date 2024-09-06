a20da6743f45ca00afcf3721726f7984ad5e2324
nvectorrr <nav2373@mail.ru>
2024-09-07 01:36:40 +0300

    new verion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,26 +8,40 @@ import {Title} from "@angular/platform-browser";
 })
 export class HomeComponent implements OnInit {
 
-  safeTypesList = ['', '1', '2', '3'];
-  safeStylesList = ['', 'Option A', 'Option B', 'Option C'];
+  safeTypesList = [{1 : 'Black'}, {2 : 'Blue'}, {3 : 'Brown'}, {4 : 'Cherry Red'}, {5 : 'Cream'}, {6 : 'Frozen Blue'}, {7 : 'Grey'}, {8 : 'Havanna Brown'}, {9 : 'Honey Yellow'}, {10 : 'Jungle Green'}, {11 : 'Lagoon Blue'}, {12 : 'Lava Orange'}, {13 : 'Lemon Yellow'}, {14 : 'Lizzard Green'}, {15 : 'Maple Green'}, {16 : 'Mint Ice'}, {17 : 'Ocean Blue'}, {18 : 'Pebble Grey'}, {19 : 'White'}];
+  safeFurnitureList = [{1 : 'Bronze'}, {2 : 'Classic'}, {3 : 'Gold'}, {4 : 'Graphite'}, {5 : 'Grey'}];
 
-  safeType : string;
-  safeStyle : string;
+  safeTypeOptions : string[];
+  safeFurnitureOptions : string[];
+
+  selectedSafeTypeOption : string;
+  selectedSafeFurnitureOptions : string;
 
   constructor(private titleService:Title) {
     this.titleService.setTitle("Configurator");
-    this.safeType = this.safeTypesList[0];
-    this.safeStyle = this.safeStylesList[0]
+
+    // сам корпус сейфа
+    this.safeTypeOptions = this.safeTypesList.map(option => Object.values(option)[0]);
+    this.selectedSafeTypeOption = this.safeTypeOptions[0];
+
+    // металлические детали
+    this.safeFurnitureOptions = this.safeFurnitureList.map(option => Object.values(option)[0]);
+    this.selectedSafeFurnitureOptions = this.safeFurnitureOptions[0];
   }
 
   ngOnInit(): void {}
 
-  onChangeSafeType(selectedType : any) {
-    if (selectedType == 1) {
-      this.safeStylesList = ['1', '2', '3'];
-    } else {
-      this.safeStylesList = ['4', '5', '6'];
-    }
-}
+  onChangeSafeType(selection : string) {
+    this.selectedSafeTypeOption = selection;
 
+    this.safeTypeOptions = this.safeTypesList.map(option => Object.values(option)[0]);
+    console.log(this.safeTypeOptions);
+  }
+
+  onChangeSafeFurniture(selection : string) {
+    this.selectedSafeFurnitureOptions = selection;
+
+    this.safeFurnitureOptions = this.safeFurnitureOptions = this.safeFurnitureList.map(option => Object.values(option)[0]);
+    console.log(this.safeFurnitureOptions);
+  }
 }
